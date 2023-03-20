1a3c192cda3c6189c544688131cfcb894aa59753
Kenneth Ekandem <kennyekandem@gmail.com>
2023-03-20 22:31:49 +0100

    first push

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Services\RabbitMQService;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post("/message", function (Request $request) {
+    $message = $_POST['message'];
+    $mqService = new RabbitMQService();
+    $mqService->publish($message);
+    return view('welcome');
+});
